b87100c7934603544e2c2e6646f675062a65ee8c
Gavin Liu <du20783@gmail.com>
2020-10-30 17:07:34 +0800

    add zh yml

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path, notice: "新增任務成功！"
+      redirect_to tasks_path, notice: I18n.t("task.create_success")
     else
       render :new
     end
@@ -26,7 +26,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to tasks_path, notice: "更新任務成功！"
+      redirect_to tasks_path, notice: I18n.t("task.update_success")
     else
       render :edit
     end
@@ -34,7 +34,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_path, alert: "刪除任務成功！"
+    redirect_to tasks_path, alert: I18n.t("task.delete_success")
   end
 
   private
