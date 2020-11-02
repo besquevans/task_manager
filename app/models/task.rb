bdc3d71afd14cbd23d3f5510ecc1fc9af1601fe1
Gavin Liu <du20783@gmail.com>
2020-11-02 12:01:06 +0800

    add validates for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :start_at, presence: true
+  validates :priority, presence: true
+  validates :status, presence: true
 end
