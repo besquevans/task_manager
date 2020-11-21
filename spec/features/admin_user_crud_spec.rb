7f9c081e419e8c141e86761f1ad4302c9f40090d
Gavin Liu <du20783@gmail.com>
2020-11-21 23:29:15 +0800

    update feature crud rspec

--- spec/features/admin_user_crud_spec.rb
+++ spec/features/admin_user_crud_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.feature "Admin User CRUD", type: :feature, driver: :chrome, js: true, slow: true  do
+  let(:user){ create(:user) }
   let(:admin_email){ "root@mail.com" }
   let(:default_email){ "test@mail.com" }
+  let(:new_user_email){ "new_user@mail.com" }
   let(:password){ "123456" }
 
   before do
@@ -13,118 +15,147 @@ RSpec.feature "Admin User CRUD", type: :feature, driver: :chrome, js: true, slow
     click_button(I18n.t("user.login"))
   end
 
-  context "index" do
-    it "find 2 users" do
-      create(:user, email: "test1@mail.com")
-      create(:user, email: "test2@mail.com")
+  describe "index" do
+    context "at begining" do
+      before do
+        user
+        visit admin_users_path
+      end
 
-      visit admin_users_path
-      expect(page).to have_content("test1@mail.com")
-      expect(page).to have_content("test2@mail.com")
+      it "should has 0 tasks" do
+        expect(find("tr", text: default_email)).to have_content("0")
+      end
     end
 
-    it "tasks count 0 at begining" do
-      user = create(:user)
-      visit admin_users_path
-      expect(find("tr", text: default_email)).to have_content("0")
-    end
+    context "2 tasks at index" do
+      it "find 2 users" do
+        create(:user, email: "test1@mail.com")
+        create(:user, email: "test2@mail.com")
 
-    it "show tasks count 2" do
-      user = create(:user)
-      create(:task, user: user)
-      create(:task, user: user)
-      visit admin_users_path
-      expect(find("tr", text: default_email)).to have_content("2")
-    end
-  end
+        visit admin_users_path
+        expect(page).to have_content("test1@mail.com")
+        expect(page).to have_content("test2@mail.com")
+      end
 
-  context "show" do
-    before do
-      create(:task, title: "task1")
-      create(:task, title: "task2")
-      visit admin_users_path
-      find("tr", text: :admin).click_link("Show")
-    end
-
-    it "show user all tasks" do
-      expect(page).to have_content("task1")
-      expect(page).to have_content("task2")
+      it "show tasks count 2" do
+        create(:task, user: user)
+        create(:task, user: user)
+        visit admin_users_path
+        expect(find("tr", text: default_email)).to have_content("2")
+      end
     end
   end
 
-  context "new and create" do
-    let(:new_user_email){ "new_user@mail.com" }
+  describe "show" do
+    context "show owner tasks" do
+      before do
+        create(:task, title: "task1")
+        create(:task, title: "task2")
+        visit admin_users_path
+        find("tr", text: :admin).click_link("Show")
+      end
 
-    before do
-      visit admin_users_path
-      click_link(I18n.t("user.new_user"))
-    end
-
-    it "create 1 user" do
-      fill_in :user_email,	with: new_user_email
-      fill_in :user_password,	with: password
-      fill_in :user_password_confirmation,	with: password
-      click_button(I18n.t(:send))
-
-      expect(page).to have_content(I18n.t("user.create_success"))
-      expect(page).to have_content(new_user_email)
-    end
-
-    it "create 1 admin" do
-      fill_in :user_email,	with: new_user_email
-      fill_in :user_password,	with: password
-      fill_in :user_password_confirmation,	with: password
-      select(I18n.t("user.role_option")[:admin], from: "user[role]")
-      click_button(I18n.t(:send))
-
-      expect(page).to have_content(I18n.t("user.create_success"))
-      expect(find("tr", text: new_user_email)).to have_content("admin")
+      it "show user all tasks" do
+        expect(page).to have_content("task1")
+        expect(page).to have_content("task2")
+      end
     end
   end
 
-  context "edit and update" do
-    before do
-      create(:user)
-      visit admin_users_path
-      find("tr", text: default_email).click_link("Edit")
+  describe "create" do
+    context "create 1 user" do
+      before do
+        visit new_admin_user_path
+        fill_in :user_email,	with: new_user_email
+        fill_in :user_password,	with: password
+        fill_in :user_password_confirmation,	with: password
+        click_button(I18n.t(:send))
+      end
+
+      it "create success" do
+        expect(page).to have_content(I18n.t("user.create_success"))
+      end
+
+      it "should has new user with email" do
+        expect(page).to have_content(new_user_email)
+      end
     end
 
-    it "update email" do
-      new_mail = "new_mail@mail.com"
-      fill_in :user_email,	with: new_mail
-      fill_in :user_password,	with: password
-      fill_in :user_password_confirmation,	with: password
-      click_button(I18n.t(:send))
+    context "create 1 admin" do
+      before do
+        visit new_admin_user_path
+        fill_in :user_email,	with: new_user_email
+        fill_in :user_password,	with: password
+        fill_in :user_password_confirmation,	with: password
+        select(I18n.t("user.role_option")[:admin], from: "user[role]")
+        click_button(I18n.t(:send))
+      end
 
-      expect(page).to have_content(I18n.t("user.update_success"))
-      expect(page).to have_content(new_mail)
-    end
-
-    it "update role" do
-      fill_in :user_password,	with: password
-      fill_in :user_password_confirmation,	with: password
-      select(I18n.t("user.role_option")[:admin], from: "user[role]")
-      click_button(I18n.t(:send))
-
-      expect(page).to have_content(I18n.t("user.update_success"))
-      expect(find("tr", text: default_email)).to have_content("admin")
+      it "should has new admin with email" do
+        expect(find("tr", text: new_user_email)).to have_content("admin")
+      end
     end
   end
 
-  context "destroy" do
-    it "destroy 1 user" do
-      selected_user = "selected_user@mail.com"
-      create(:user, email: selected_user)
-      create(:user)
+  describe "update" do
+    context "update user email" do
+      let(:new_email){ "new_email@mail.com" }
 
-      visit admin_users_path
-      expect(page).to have_content(default_email)
-      expect(page).to have_content(selected_user)
-      find("tr", text: selected_user).click_link("Destroy")
-      page.driver.browser.switch_to.alert.accept #確認刪除
-      expect(page).to have_content(I18n.t("user.delete_success"))
-      expect(page).to have_content(default_email)
-      expect(page).to have_no_content(selected_user)
+      before do
+        visit edit_admin_user_path(user)
+        fill_in :user_email,	with: new_email
+        fill_in :user_password,	with: password
+        fill_in :user_password_confirmation,	with: password
+        click_button(I18n.t(:send))
+      end
+
+      it "update succes" do
+        expect(page).to have_content(I18n.t("user.update_success"))
+      end
+
+      it "should has new email" do
+        expect(page).to have_content(new_email)
+      end
+    end
+
+    context "update user role" do
+      before do
+        visit edit_admin_user_path(user)
+        fill_in :user_password,	with: password
+        fill_in :user_password_confirmation,	with: password
+        select(I18n.t("user.role_option")[:admin], from: "user[role]")
+        click_button(I18n.t(:send))
+      end
+
+      it "has new role" do
+        expect(find("tr", text: default_email)).to have_content("admin")
+      end
+    end
+  end
+
+  describe "destroy" do
+    context "destroy 1 user" do
+      let(:selected_user){ "selected_user@mail.com" }
+
+      before do
+        create(:user, email: selected_user)
+        create(:user)
+        visit admin_users_path
+        find("tr", text: selected_user).click_link("Destroy")
+        page.driver.browser.switch_to.alert.accept #確認刪除
+      end
+
+      it "destroy success" do
+        expect(page).to have_content(I18n.t("user.delete_success"))
+      end
+
+      it "should keep other user" do
+        expect(page).to have_content(default_email)
+      end
+
+      it "shold has no selected user" do
+        expect(page).to have_no_content(selected_user)
+      end
     end
   end
 end
--- spec/features/task_crud_spec.rb
+++ spec/features/task_crud_spec.rb
@@ -1,102 +1,153 @@
 require "rails_helper"
 
 RSpec.feature "Read task", type: :feature, driver: :chrome, js: true, slow: true  do
-  before(:each) do
-    visit sign_up_users_path
-    fill_in :user_email,	with: "user1@mail.com"
-    fill_in :user_password,	with: "123456"
-    fill_in :user_password_confirmation,	with: "123456"
-    click_button(I18n.t("user.sign_up"))
-    @user = User.first
+  let(:default_email){ "test@mail.com" }
+  let(:password){ "123456" }
+  let(:test_task_title){ "Test Task" }
+
+  before do
+    create(:user)
+    visit sign_in_users_path
+    fill_in :user_email,	with: default_email
+    fill_in :user_password,	with: password
+    click_button(I18n.t("user.login"))
   end
 
-  context "index" do
-    it "create 2 tasks" do
-      task1 = create(:task, user: @user)
-      task2 = create(:task, user: @user)
+  describe "index" do
+    context "create 2 tasks" do
+      before do
+        create(:task, title: "task1")
+        create(:task, title: "task2")
 
-      expect(Task.all.to_a).to eq([task1, task2])
-    end
+        other = create(:user, email: "other@mail.com")
+        other_task = create(:task, user: other, title: "Other Task")
+        visit tasks_path
+      end
 
-    it "read 2 tasks" do
-      task1 = create(:task, user: @user)
-      task2 = create(:task, user: @user)
-      visit tasks_path
+      it "has 2 tasks" do
+        expect(page).to have_content("task1")
+        expect(page).to have_content("task2")
+      end
 
-      expect(page).to have_content("Test Task", count: 2)
-    end
-
-    it "read only self tasks" do
-      my_task = create(:task, user: @user, title: "My Task")
-      other = create(:user)
-      other_task = create(:task, user: other, title: "Other Task")
-
-      visit tasks_path
-      expect(page).to have_content("My Task", count: 1)
-    end
-  end
-
-  context "show" do
-    it "read selected task" do
-      task = create(:task, user: @user)
-      selected_task = create :task, title: "Selected Task"
-      task = create(:task, user: @user)
-      visit task_path(selected_task.id)
-      expect(page).to have_content("Selected Task")
+      it "has only self tasks" do
+        expect(page).to have_no_content("Other Task")
+      end
     end
   end
 
-  context "new and create" do
-    it "create task" do
-      visit new_task_path
-      fill_in :task_title,	with: "New Task"
-      fill_in :task_content,	with: "New Content"
-      fill_in :task_start_at,	with: Time.now
-      fill_in :task_end_at,	with: Time.now + 1.day
-      #高
-      select(I18n.t("task.priority_option")[0], from: "form-priority")
-      #進行中
-      select(I18n.t("task.status_option")[1], from: "form-status")
-      #送出
-      click_button(I18n.t(:send))
-      #notice 新增任務成功！
-      expect(page).to have_content(I18n.t("task.create_success"))
+  describe "show" do
+    context "at selected task show" do
+      before do
+        task = create(:task)
+        selected_task = create :task, title: "Selected Task"
+        task = create(:task)
+        visit task_path(selected_task.id)
+      end
+
+      it "has selected task" do
+        expect(page).to have_content("Selected Task")
+      end
     end
   end
 
-  context "edit and update" do
-    it "update task" do
-      task = create(:task, user: @user)
-      visit edit_task_path(task)
-      fill_in :task_title,	with: "New Task"
-      fill_in :task_content,	with: "New Content"
-      fill_in :task_start_at,	with: Time.now
-      fill_in :task_end_at,	with: Time.now + 1.day
-      #高
-      select(I18n.t("task.priority_option")[0], from: "form-priority")
-      #進行中
-      select(I18n.t("task.status_option")[1], from: "form-status")
-      #送出
-      click_button(I18n.t(:send))
-      #notice 更新任務成功！
-      expect(page).to have_content(I18n.t("task.update_success"))
+  describe "create" do
+    context "create new task" do
+      let(:new_task_title){ "New Task" }
+
+      before do
+        visit new_task_path
+        fill_in :task_title,	with: new_task_title
+        fill_in :task_content,	with: "New Task Content"
+        fill_in :task_start_at,	with: Time.now
+        fill_in :task_end_at,	with: Time.now + 1.day
+        #低
+        select(I18n.t("task.priority_option")[0], from: "form-priority")
+        #進行中
+        select(I18n.t("task.status_option")[1], from: "form-status")
+        #送出
+        click_button(I18n.t(:send))
+      end
+
+      it "should create success" do
+        #notice 新增任務成功！
+        expect(page).to have_content(I18n.t("task.create_success"))
+      end
+
+      it "should has new title" do
+        expect(page).to have_content(new_task_title)
+      end
+
+      it "should has new priority" do
+        expect(page).to have_content(I18n.t("task.priority_option")[0])
+      end
+
+      it "should has new status" do
+        expect(page).to have_content(I18n.t("task.status_option")[1])
+      end
     end
   end
 
-  context "destory" do
-    it "destory task" do
-      task = create(:task, user: @user)
-      selected_task = create :task, title: "Selected Task"
-      task = create(:task, user: @user)
-      visit tasks_path
-      expect(page).to have_content("Test Task", count: 2)
-      expect(page).to have_content("Selected Task", count: 1)
-      find("tr", text: "Selected Task").click_link("Destroy")
-      page.driver.browser.switch_to.alert.accept #確認刪除
-      expect(page).to have_content("Test Task", count: 2)
-      expect(page).to have_no_content("Selected Task")
-      #notice 刪除任務成功！
-      expect(page).to have_content(I18n.t("task.delete_success"))
+  describe "update" do
+    context "update 1 task" do
+      let(:renew_task_title){ "Renew Task Title" }
+
+      before do
+        task = create(:task)
+        visit edit_task_path(task)
+        fill_in :task_title,	with: renew_task_title
+        fill_in :task_content,	with: "Renew Task Content"
+        fill_in :task_start_at,	with: Time.now
+        fill_in :task_end_at,	with: Time.now + 1.day
+        #高
+        select(I18n.t("task.priority_option")[2], from: "form-priority")
+        #完成
+        select(I18n.t("task.status_option")[2], from: "form-status")
+        #送出
+        click_button(I18n.t(:send))
+      end
+
+      it "update task" do
+        #notice 更新任務成功！
+        expect(page).to have_content(I18n.t("task.update_success"))
+      end
+
+      it "should has updated title" do
+        expect(page).to have_content(renew_task_title)
+      end
+
+      it "should has updated priority" do
+        expect(page).to have_content(I18n.t("task.priority_option")[2])
+      end
+
+      it "should has updated status" do
+        expect(page).to have_content(I18n.t("task.status_option")[2])
+      end
+    end
+  end
+
+  describe "destroy" do
+    context "destory 1 task" do
+      before do
+        task = create(:task)
+        selected_task = create :task, title: "Selected Task"
+        task = create(:task)
+        visit tasks_path
+        find("tr", text: "Selected Task").click_link("Destroy")
+        page.driver.browser.switch_to.alert.accept #確認刪除
+      end
+
+      it "should destroy success" do
+        #notice 刪除任務成功！
+        expect(page).to have_content(I18n.t("task.delete_success"))
+      end
+
+      it "keep other tasks" do
+        expect(page).to have_content("Test Task", count: 2)
+      end
+
+      it "remove selected task" do
+        expect(page).to have_no_content("Selected Task")
+      end
     end
   end
 end
